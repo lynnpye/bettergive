6f21ee2d386b6dd05026205afc53bb2d25193d92
Lynn Pye <github@pyehouse.com>
2022-08-24 13:39:35 -0500

    improved search capability

--- README.md
+++ README.md
@@ -17,3 +17,9 @@ So if you start typing e.g.:
 You will be presented with a suggestion list of any item whose resource location includes the substring 'sword'. This could be 'minecraft:iron_sword', 'minecraft:stone_sword', or, for our example, 'coolswords:wool_sword' or 'coolswords:slime_sword' (assuming such a mod with such items was installed).
 
 No more having to know beforehand the modid of the mod with the item you want; no more having to type in the modid manually before you can start seeing all of the items matching your search.
+
+New! Did you start searching for a pressure plate and then realized you wanted a particular type of wood? You can now use irregular characters to do a "multi-part" search. For example:
+
+  /bgive @s press*aca
+
+This will present to you anything with either "press" or "aca" in the name.
--- src/main/java/com/pyehouse/mcmods/bettergive/common/BetterGiveCommandHandler.java
+++ src/main/java/com/pyehouse/mcmods/bettergive/common/BetterGiveCommandHandler.java
@@ -8,7 +8,9 @@ import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.suggestion.SuggestionProvider;
 import net.minecraft.command.CommandSource;
 import net.minecraft.command.Commands;
-import net.minecraft.command.arguments.*;
+import net.minecraft.command.arguments.EntityArgument;
+import net.minecraft.command.arguments.ItemArgument;
+import net.minecraft.command.arguments.ItemInput;
 import net.minecraft.command.impl.GiveCommand;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.util.ResourceLocation;
@@ -30,6 +32,8 @@ public class BetterGiveCommandHandler {
     public static final String ARG_players = "players";
     public static final String ARG_item = "item";
     public static final String ARG_count = "count";
+
+    public static final String RESOURCE_LOCATION_CHARACTERS_DISALLOWED = "[^a-z0-9\\/._-]";
 
     @SubscribeEvent
     public static void onRegisterCommand(RegisterCommandsEvent event) {
@@ -62,9 +66,18 @@ public class BetterGiveCommandHandler {
     private static SuggestionProvider<CommandSource> createSuggester() {
         return (context, builder) -> {
             String stringRemainder = builder.getRemaining().toLowerCase(Locale.ROOT);
+            String[] bitParts = stringRemainder.split(RESOURCE_LOCATION_CHARACTERS_DISALLOWED, 0);
             for (ResourceLocation resourceLocation : ForgeRegistries.ITEMS.getKeys()) {
                 String resourceString = resourceLocation.toString();
-                if (resourceString.contains(stringRemainder)) {
+                boolean addSuggestion = true;
+                for (String part : bitParts) {
+                    if (!resourceString.contains(part)) {
+                        // skip this resourceLocation
+                        addSuggestion = false;
+                        break;
+                    }
+                }
+                if (addSuggestion) {
                     builder.suggest(resourceString);
                 }
             }
